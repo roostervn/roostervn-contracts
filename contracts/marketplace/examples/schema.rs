764cf9ffc847572d92c7c8e1db23b6418732b6ee
sccvn <kevinnguyen.asian.vie@gmail.com>
2022-07-07 10:27:06 +0000

    export schema for marketplace

--- contracts/marketplace/examples/schema.rs
+++ contracts/marketplace/examples/schema.rs
@@ -3,8 +3,9 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use marketplace::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
+use marketplace::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg, HandleMsg, InitMsg, BuyNft, SellNft};
 use marketplace::state::State;
+use marketplace::package::{ContractInfoResponse, QueryOfferingResult, OfferingResponse };
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -17,4 +18,12 @@ fn main() {
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(State), &out_dir);
     export_schema(&schema_for!(CountResponse), &out_dir);
+    export_schema(&schema_for!(InitMsg), &out_dir);
+    export_schema(&schema_for!(HandleMsg), &out_dir);
+    export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(SellNft), &out_dir);
+    export_schema(&schema_for!(BuyNft), &out_dir);
+    export_schema(&schema_for!(OfferingResponse), &out_dir);
+    export_schema(&schema_for!(ContractInfoResponse), &out_dir);
+    export_schema(&schema_for!(QueryOfferingResult), &out_dir);
 }
